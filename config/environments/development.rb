5b4c269da56249a1d7664466a861298c283955a7
minooor <minorilax13@gmail.com>
2022-04-30 07:47:44 +0000

    Add ゲストログイン機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,8 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
-  config.hosts << "6f2139a9c44442549bdd56a3bdcd1fb6.vfs.cloud9.us-east-1.amazonaws.com"
-
+  config.hosts << "20e355ddb9d1483c80e7cc19a0fe4191.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
